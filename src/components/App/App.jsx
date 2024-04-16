e0d28a5951f581f701aa0d629c88581e90e31ee4
SergeyPanchenko72 <143348555+SergeyPanchenko72@users.noreply.github.com>
2024-04-16 22:38:29 +0300

    add find

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@ import css from "./App.module.css";
 import { useState } from "react";
 import initialContacts from "../../startContacts.json";
 import ContactList from "../ContactList/ContactList";
-// import SearchBox from "../SearchBox/";
+import SearchBox from "../SearchBox/SearchBox";
 import ContactForm from "../ContactForm/ContactForm";
 import * as Yup from "yup";
 
@@ -14,6 +14,7 @@ function App() {
   };
 
   const [contacts, setContacts] = useState(initialContacts);
+  const [search, setSearch] = useState("");
 
   const addContact = (newContact) => {
     setContacts((prevContacts) => {
@@ -36,17 +37,21 @@ function App() {
       .matches(/^\d{3}-\d{2}-\d{2}$/, "Invalid phone number format")
       .required("Required"),
   });
+  const visibleContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className={css.container}>
       <h1>Phonebook</h1>
+      <p>{search}</p>
       <ContactForm
         initialValues={initialValues}
         onAdd={addContact}
         FeedbackSchema={FeedbackSchema}
       />
-      {/* <SearchBox /> */}
-      <ContactList contacts={contacts} onDelete={delContact} />
+      <SearchBox value={search} onSearch={setSearch} />
+      <ContactList contacts={visibleContacts} onDelete={delContact} />
     </div>
   );
 }
