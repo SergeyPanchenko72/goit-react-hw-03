d50c624da7060fd8a367675c9acd5267c351813d
SergeyPanchenko72 <143348555+SergeyPanchenko72@users.noreply.github.com>
2024-04-17 15:07:05 +0300

    add icon

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,20 @@
 import css from "./Contact.module.css";
+import { BsFillPersonFill } from "react-icons/bs";
+import { BsTelephoneFill } from "react-icons/bs";
 
 export default function Contact({ data: { id, name, number }, onDelete }) {
   return (
     <div className={css.container}>
       <div>
-        <p>{name}</p>
-        <p>{number}</p>
+        <p>
+          <BsFillPersonFill className={css.icon} />
+          {name}
+        </p>
+
+        <p>
+          <BsTelephoneFill className={css.icon} />
+          {number}
+        </p>
       </div>
       <button type="button" className={css.button} onClick={() => onDelete(id)}>
         Delete
